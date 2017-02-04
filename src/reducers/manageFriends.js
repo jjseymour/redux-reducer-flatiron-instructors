42a2515b5ac315746a2ac3981f35bbfd94df7712
JJ Seymour <jjseymour@gmail.com>
2017-02-04 16:11:37 -0500

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {friends: [...state.friends, action.payload], numberOfPresents: state.numberOfPresents}
+    case "REMOVE_FRIEND":
+      let newFriend = state.friends.filter((friend)=>{
+      return friend.id !== action.payload
+      })
+      return {friends: newFriend, numberOfPresents: state.numberOfPresents}
+    default:
+      return state 
+  }
 }
